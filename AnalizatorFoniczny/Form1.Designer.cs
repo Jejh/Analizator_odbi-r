656fdb968206a179bb6a87ba9f0641386cd8fffc
Jejh <thomsono@gmail.com>
2017-02-13 20:48:45 +0100

    Add files via upload

---
+++ AnalizatorFoniczny/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace AnalizatorFoniczny
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.cb_Ports = new System.Windows.Forms.ComboBox();
+            this.lb_Port = new System.Windows.Forms.Label();
+            this.lb_BaudRate = new System.Windows.Forms.Label();
+            this.pb_connect = new System.Windows.Forms.ProgressBar();
+            this.SerialP = new System.IO.Ports.SerialPort(this.components);
+            this.cb_BaudRate = new System.Windows.Forms.ComboBox();
+            this.lb_Chart = new System.Windows.Forms.Label();
+            this.bt_do = new System.Windows.Forms.Button();
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cb_Ports
+            // 
+            this.cb_Ports.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_Ports.FormattingEnabled = true;
+            this.cb_Ports.Location = new System.Drawing.Point(273, 14);
+            this.cb_Ports.Name = "cb_Ports";
+            this.cb_Ports.Size = new System.Drawing.Size(90, 21);
+            this.cb_Ports.TabIndex = 2;
+            // 
+            // lb_Port
+            // 
+            this.lb_Port.AutoSize = true;
+            this.lb_Port.Location = new System.Drawing.Point(229, 17);
+            this.lb_Port.Name = "lb_Port";
+            this.lb_Port.Size = new System.Drawing.Size(29, 13);
+            this.lb_Port.TabIndex = 3;
+            this.lb_Port.Text = "Port:";
+            // 
+            // lb_BaudRate
+            // 
+            this.lb_BaudRate.AutoSize = true;
+            this.lb_BaudRate.Location = new System.Drawing.Point(408, 17);
+            this.lb_BaudRate.Name = "lb_BaudRate";
+            this.lb_BaudRate.Size = new System.Drawing.Size(61, 13);
+            this.lb_BaudRate.TabIndex = 4;
+            this.lb_BaudRate.Text = "Baud Rate:";
+            // 
+            // pb_connect
+            // 
+            this.pb_connect.Location = new System.Drawing.Point(86, 12);
+            this.pb_connect.Name = "pb_connect";
+            this.pb_connect.Size = new System.Drawing.Size(34, 23);
+            this.pb_connect.TabIndex = 6;
+            // 
+            // SerialP
+            // 
+            this.SerialP.PortName = "COM3";
+            this.SerialP.ReadBufferSize = 100000;
+            this.SerialP.WriteBufferSize = 128;
+            this.SerialP.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialP_DataReceived);
+            // 
+            // cb_BaudRate
+            // 
+            this.cb_BaudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_BaudRate.FormattingEnabled = true;
+            this.cb_BaudRate.Items.AddRange(new object[] {
+            "2400",
+            "4800",
+            "9600",
+            "19200"});
+            this.cb_BaudRate.Location = new System.Drawing.Point(484, 14);
+            this.cb_BaudRate.Name = "cb_BaudRate";
+            this.cb_BaudRate.Size = new System.Drawing.Size(90, 21);
+            this.cb_BaudRate.TabIndex = 9;
+            // 
+            // lb_Chart
+            // 
+            this.lb_Chart.AutoSize = true;
+            this.lb_Chart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lb_Chart.Location = new System.Drawing.Point(716, 44);
+            this.lb_Chart.Name = "lb_Chart";
+            this.lb_Chart.Size = new System.Drawing.Size(276, 24);
+            this.lb_Chart.TabIndex = 20;
+            this.lb_Chart.Text = "Widmo próbkowanego sygnału";
+            // 
+            // bt_do
+            // 
+            this.bt_do.Location = new System.Drawing.Point(126, 12);
+            this.bt_do.Name = "bt_do";
+            this.bt_do.Size = new System.Drawing.Size(75, 23);
+            this.bt_do.TabIndex = 21;
+            this.bt_do.Text = "Start";
+            this.bt_do.UseVisualStyleBackColor = true;
+            this.bt_do.Click += new System.EventHandler(this.bt_do_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(45, 104);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Widmo";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1367, 422);
+            this.chart1.TabIndex = 22;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1638, 848);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.bt_do);
+            this.Controls.Add(this.lb_Chart);
+            this.Controls.Add(this.cb_BaudRate);
+            this.Controls.Add(this.pb_connect);
+            this.Controls.Add(this.lb_BaudRate);
+            this.Controls.Add(this.lb_Port);
+            this.Controls.Add(this.cb_Ports);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+        //    ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox cb_Ports;
+        private System.Windows.Forms.Label lb_Port;
+        private System.Windows.Forms.Label lb_BaudRate;
+        private System.Windows.Forms.ProgressBar pb_connect;
+        private System.IO.Ports.SerialPort SerialP;
+        private System.Windows.Forms.ComboBox cb_BaudRate;
+        private System.Windows.Forms.Label lb_Chart;
+        private System.Windows.Forms.Button bt_do;
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ AnalizatorFoniczny/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace AnalizatorFoniczny
+{
+    public partial class Form1 : Form
+    {
+        private byte[] bArray = new byte[20 * numberOfSamples + 4]; // frequencies (512 samples) and magnitudes (512 values) - each is a single precision float so 4 bytes
+        private Single[] fArray = new float[2 * numberOfSamples + 1]; // float data array
+        //private byte[] bBuffer = new byte[4];
+        private byte[] bSterowanie = new byte[1];
+
+        private static int counterOfRecBytes = 0;
+        private static int numberOfSamples = 512;
+
+
+        public Form1()
+        {
+            InitializeComponent();                        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // UI settings
+            //chart1.ChartAreas[0].AxisX.Minimum = 0;
+            //schart1.ChartAreas[0].AxisX.Maximum = 30000;
+
+            // Default transmission parameters for testing
+            SerialP.PortName = "COM5";
+            SerialP.BaudRate = 115200;
+            SerialP.StopBits = StopBits.One;
+            SerialP.DataBits = 8;
+            SerialP.Parity = Parity.None;
+            SerialP.Handshake = Handshake.None;
+            SerialP.ReadBufferSize = 16 * numberOfSamples + 4;
+            
+        }
+
+        /// <summary>
+        /// SerialPort Data Recieved Event Handler
+        /// </summary>
+        
+        private void SerialP_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+
+            
+            int previouse = counterOfRecBytes;
+
+            counterOfRecBytes += sp.BytesToRead;
+
+            sp.Read(bArray, previouse, (counterOfRecBytes - previouse));
+
+            if (counterOfRecBytes >= 8 * numberOfSamples + 4)
+            {
+                decimal[] decArray = new decimal[2 * numberOfSamples + 1];
+                for (uint i = 0; i < 2 * numberOfSamples + 1; i++)
+                {
+                    fArray[i] = Math.Abs(ByteToFloat(bArray, i));
+                    //decArray[i] = (Decimal)fArray[i];
+                    if (fArray[i] > (Single)decimal.MaxValue || fArray[i] < (Single)decimal.MinValue) fArray[i] = 0;
+
+                    //if (Single.IsNaN(fArray[i]) || Single.IsInfinity(fArray[i])) fArray[i] = 0;
+                }
+
+                if (InvokeRequired)
+                {
+                    Invoke(new MethodInvoker(ChartData));
+                }
+                else
+                {
+                    ChartData();
+                }
+
+                // Set counterOfRecBytes to recieve new data
+                counterOfRecBytes = 0;
+            }
+
+
+
+        }
+
+        ///// <summary>
+        ///// Changes my recieved bArray to single precision floats
+        ///// </summary>
+        private float ByteToFloat(byte[] input, UInt32 i)
+        {
+            byte[] array = new[] { input[4 * i], input[4 * i + 1], input[4 * i + 2], input[4 * i + 3] };
+            return BitConverter.ToSingle(array, 0);
+        }
+
+        /// <summary>
+        /// Setting chart data
+        /// </summary>
+        private void ChartData()
+        {
+
+            chart1.Series["Widmo"].Points.Clear();
+
+            for(int i = 1; i < numberOfSamples + 1; i++)
+            {
+                chart1.Series["Widmo"].Points.AddXY(fArray[i], fArray[i + numberOfSamples]);
+            }
+                       
+        }
+
+        private void bt_do_Click(object sender, EventArgs e)
+        {
+            if (bt_do.Text == "Start")
+            {
+                bSterowanie[0] = 10;
+                bt_do.Text = "Stop";
+                pb_connect.Value = 100;
+                Thread.SpinWait(200);
+                SerialP.Open();
+                SerialP.Write(bSterowanie, 0, 1); //send 'a' character to start                
+            }
+            else
+            {
+                bSterowanie[0] = 150;
+                bt_do.Text = "Start";
+                SerialP.Write(bSterowanie, 0, 1); //send 'z' character to stop
+                pb_connect.Value = 0;
+                Thread.SpinWait(100);
+                SerialP.Close();
+
+            }
+        }
+    }
+
+}
